c359a919af84317cb62f93b9c70857ea31055d87
KAMARA Yasmine <CDN001@Dpigierci.edu>
2025-09-15 20:01:27 +0200

    premier

---
+++ app/src/main/java/com/example/gestionnaireadou/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.gestionnaireadou
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.gestionnaireadou.ui.theme.GestionnaireadouTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GestionnaireadouTheme {
+                Surface {
+                    GestionnaireApp(
+                        stringResource(R.string.task),
+                        stringResource(R.string.work),
+                        modifier = Modifier
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GestionnaireApp(text1: String,text2: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.gestionnaire)
+
+    Column(
+        verticalArrangement=Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()) {
+        Image(
+            painter = image,
+            contentDescription = null,
+
+
+            )
+        Text(
+            text = text1,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+
+        Text(
+            text = text2,
+            fontSize= 16.sp,
+            modifier = modifier
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GestionnaireadouTheme {
+        GestionnaireApp(
+            stringResource(R.string.task),
+            stringResource(R.string.work),
+            modifier = Modifier
+        )
+    }
+}
